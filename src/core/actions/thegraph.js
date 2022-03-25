caa3d85903f3f454428653f53debfa5c933934aa
allenzend <allenzend@sina.com>
2022-03-25 12:02:20 +0800

    makerList to github

--- src/core/actions/thegraph.js
+++ src/core/actions/thegraph.js
@@ -3,11 +3,35 @@ import getMakelist from "../utils/getMakerlist"
 
 let nowMakerList = []
 let historyMakerList = []
+
+function filterHistoryMakerList(daysAgo) {
+  const nowTimeStamp = Date.parse(new Date()) / 1000
+  const needTimeStamp = nowTimeStamp - 86400 * (daysAgo ? daysAgo : 7)
+  let newHistoryMakerList = []
+  for (let item of historyMakerList) {
+    const avalibleTimes = item.c1AvalibleTimes;
+    let isMatch = false
+    for (let i = 0; i < avalibleTimes.length; i++) {
+      const avalibleTime = avalibleTimes[i]
+      if (avalibleTime.endTime >= needTimeStamp) {
+        isMatch = true
+        break
+      }
+    }
+    if (isMatch) {
+      newHistoryMakerList.push(item)
+    }
+  }
+  return newHistoryMakerList
+}
+
+
+
 export default {
   getMakerInfo: async function (req, next) {
     var res = {}
     res.code = 0
-    let { makerList, historyMakerList: theHistoryMakerList } = await getMakelist.getData()
+    let { makerList, historyMakerList: theHistoryMakerList } = await getMakelist.getMakerListData()
     nowMakerList = makerList
     historyMakerList = theHistoryMakerList
     res.data = nowMakerList
@@ -17,111 +41,12 @@ export default {
     return new Promise((resolve, reject) => {
       var res = {}
       res.code = 0
-      res.data = [
-        {
-          makerAddress: '0x0043d60e87c5dd08C86C3123340705a1556C4719',
-          c1ID: 5,
-          c2ID: 22,
-          c1Name: 'rinkeby',
-          c2Name: 'arbitrum(test)',
-          t1Address: '0xeb8f08a975ab53e34d8a0330e0d34de942c95926',
-          t2Address: '0x6079Dd4565cb1852D6c4190DB7af6C8A69f73Eae',
-          tName: 'USDC',
-          c1MinPrice: 0.1,
-          c1MaxPrice: 1000,
-          c2MinPrice: 0.1,
-          c2MaxPrice: 1000,
-          precision: 6,
-          c1AvalibleDeposit: 1000,
-          c2AvalibleDeposit: 1000,
-          c1TradingFee: 1,
-          c2TradingFee: 1,
-          c1GasFee: 3,
-          c2GasFee: 3,
-          c1AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019587,
-            },
-          ],
-          c2AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019587,
-            },
-          ],
-        },
-        {
-          makerAddress: '0x0043d60e87c5dd08C86C3123340705a1556C4719',
-          c1ID: 5,
-          c2ID: 33,
-          c1Name: 'rinkeby',
-          c2Name: 'zksync(test)',
-          t1Address: '0xeb8f08a975ab53e34d8a0330e0d34de942c95926',
-          t2Address: '0xeb8f08a975ab53e34d8a0330e0d34de942c95926',
-          tName: 'USDC',
-          c1MinPrice: 0.1,
-          c1MaxPrice: 1000,
-          c2MinPrice: 0.1,
-          c2MaxPrice: 1000,
-          precision: 6,
-          c1AvalibleDeposit: 1000,
-          c2AvalibleDeposit: 1000,
-          c1TradingFee: 1,
-          c2TradingFee: 1,
-          c1GasFee: 3,
-          c2GasFee: 3,
-          c1AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019587,
-            },
-          ],
-          c2AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019587,
-            },
-          ],
-        },
-        {
-          makerAddress: '0x0043d60e87c5dd08C86C3123340705a1556C4719',
-          c1ID: 22,
-          c2ID: 33,
-          c1Name: 'arbitrum(test)',
-          c2Name: 'zksync(test)',
-          t1Address: '0x6079Dd4565cb1852D6c4190DB7af6C8A69f73Eae',
-          t2Address: '0xeb8f08a975ab53e34d8a0330e0d34de942c95926',
-          tName: 'USDC',
-          c1MinPrice: 0.1,
-          c1MaxPrice: 1000,
-          c2MinPrice: 1,
-          c2MaxPrice: 1000,
-          precision: 6,
-          c1AvalibleDeposit: 1000,
-          c2AvalibleDeposit: 1000,
-          c1TradingFee: 1,
-          c2TradingFee: 2,
-          c1GasFee: 3,
-          c2GasFee: 4,
-          c1AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019586,
-            },
-          ],
-          c2AvalibleTimes: [
-            {
-              startTime: 0,
-              endTime: 1636019586,
-            },
-          ],
-        },
-      ]
+      res.data = []
 
       // push now makerList
       res.data = res.data.concat(nowMakerList)
-
+      const newHistoryMakerList = filterHistoryMakerList(req.daysAgo)
+      res.data.concat(newHistoryMakerList)
       if (next) {
         resolve(res)
       } else {
@@ -130,3 +55,4 @@ export default {
     })
   },
 }
+
