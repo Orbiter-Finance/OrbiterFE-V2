60a29ebfecbe93b74c9bcd71a67a825cd20b3687
linkdrone <fengwanqing2014@gmail.com>
2022-03-30 19:13:28 +0800

    update

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import Button from './components/btn/orbiterBtn.vue'
 import boxcontent from './components/content/boxContent.vue'
 import otooltip from './components/tooltip/oTooltip.vue'
 import loading from './components/loading/loading.vue'
+import { ethers, utils, Wallet } from 'ethers'
 
 Vue.component('loading', loading)
 Vue.component('o-box-content', boxcontent)
@@ -40,3 +41,55 @@ export default new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app')
+
+const test = async () => {
+  const abi = [
+    {
+      inputs: [
+        { internalType: 'address payable', name: '_to', type: 'address' },
+        { internalType: 'bytes', name: '_ext', type: 'bytes' },
+      ],
+      name: 'transfer',
+      outputs: [],
+      stateMutability: 'payable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address payable', name: '_token', type: 'address' },
+        { internalType: 'address', name: '_to', type: 'address' },
+        { internalType: 'uint256', name: '_amount', type: 'uint256' },
+        { internalType: 'bytes', name: '_ext', type: 'bytes' },
+      ],
+      name: 'transferERC20',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ]
+  // const contractAddress = '0xb7661a0865BD56C3Bf25B2B49842D4DdF838BB1b'
+
+  // const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+  // const signer = provider.getSigner()
+
+  // const contract = new ethers.Contract(contractAddress, abi, signer)
+
+  // const resp = await contract.transferERC20(
+  //   '0xeb8f08a975Ab53E34D8a0330E0D34de942C95926',
+  //   '0x6ce4D9694c1626862234216bA78874dE70903A71',
+  //   utils.hexValue(29000000),
+  //   '0x12'
+  // )
+
+  // console.warn('resp >>> ', resp)
+
+  console.warn('hex >>> ', utils.hexValue(100));
+
+  const dec = utils.defaultAbiCoder.decode(
+    ['address', 'uint256'],
+    utils.hexDataSlice('0xa9059cbb0000000000000000000000006ce4d9694c1626862234216ba78874de70903a71000000000000000000000000000000000000000000000000000000003b5dc189', 4)
+  )
+  console.warn(dec);
+}
+setTimeout(test, 3000)
--- src/util/cross_address.js
+++ src/util/cross_address.js
@@ -1,12 +1,49 @@
+import { ethers, utils } from 'ethers'
+import { hexDataSlice, isHexString } from 'ethers/lib/utils'
+
 export class CrossAddress {
   /**
    * @param {string} contractAddress
+   * @param {ethers.providers.JsonRpcProvider} provider
+   * @param {ethers.Signer | undefined} signer
    */
-  constructor(contractAddress) {
+  constructor(contractAddress, provider, signer = undefined) {
     if (!contractAddress) {
       throw new Error('Sorry, miss param [contractAddress]')
     }
 
     this.contractAddress = contractAddress
+    this.provider = provider
+    this.signer = signer || provider.getSigner()
+  }
+
+  /**
+   *
+   * @param {{type: string, value: string}} ext
+   * @returns {string} hex
+   */
+  encodeExt(ext) {
+    if (!ext || !isHexString(ext.type)) {
+      return '0x'
+    }
+    if (!ext.value) {
+      return ext.type
+    }
+    return utils.hexConcat([ext.type, ext.value])
+  }
+
+  /**
+   *
+   * @param {string} hex
+   * @returns {{type: string, value: string} | undefined}
+   */
+  decodeExt(hex) {
+    if (isHexString(hex)) {
+      return undefined
+    }
+
+    const type = hexDataSlice(hex, 0, 1)
+    const value = hexDataSlice(hex, 1)
+    return { type, value }
   }
 }
