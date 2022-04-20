9a3ac55a48ad1f3d828cabd448f946e857b3df40
Eric <zu19890223@gmail.com>
2022-04-20 18:05:54 +0800

    modify makerList origin

--- src/core/utils/getMakerlist.js
+++ src/core/utils/getMakerlist.js
@@ -1,30 +1,31 @@
 import axios from 'axios'
-const apiUrl = "https://maker-list.s3.amazonaws.com"
+const apiUrl = 'https://orbiter-makerlist.s3.ap-northeast-1.amazonaws.com'
+
 async function getNewMakerList(count = 0) {
-    try {
-        return await getNewMakerListOnce()
-    } catch (error) {
-        errorLogger.error(
-            `getNewMakerList error=${error.message},try again ${count}`
-        )
-        count++
-        if (count < 5) {
-            return await getNewMakerList(count)
-        }
+  try {
+    return await getNewMakerListOnce()
+  } catch (error) {
+    errorLogger.error(
+      `getNewMakerList error=${error.message},try again ${count}`
+    )
+    count++
+    if (count < 5) {
+      return await getNewMakerList(count)
     }
+  }
 }
 
 async function getNewMakerListOnce() {
-    const res = await axios({
-        url: `${apiUrl}/maker_list.json`,
-        method: "get"
-    });
-    if (res.status == 200) {
-        return { ...res.data }
-    } else {
-        throw new Error("pull maker_list nothing")
-    }
+  const res = await axios({
+    url: `${apiUrl}/rinkeby/makerList.json`,
+    method: 'get',
+  })
+  if (res.status == 200) {
+    return { ...res.data }
+  } else {
+    throw new Error('pull maker_list nothing')
+  }
 }
 export default {
-    getNewMakerList
+  getNewMakerList,
 }
