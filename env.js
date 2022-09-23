6e3011de7b66dd19a4b13a590a8f2de4620dffb6
kakui <huanhui.boy@gmail.com>
2022-09-23 17:03:45 +0800

    update hostory api

--- env.js
+++ env.js
@@ -1,11 +1,10 @@
 const baseUrl = `https://twy7zbm79a.execute-api.ap-northeast-1.amazonaws.com`
 const baseDomain = `https://api.orbiter.finance`
-const historyApiUrl = `${baseDomain}/api`
 export const netStateUrl = `${baseDomain}`
 export default {
   // baseUrl: process.env.NODE_ENV === 'production' ? 'https://twy7zbm79a.execute-api.ap-northeast-1.amazonaws.com' : '/send',
   baseUrl,
-  baseTraddingUrl:  process.env.NODE_ENV === 'production' ? historyApiUrl : 'http://ec2-54-238-20-18.ap-northeast-1.compute.amazonaws.com:3003/api',
+  baseTraddingUrl: 'http://ec2-54-238-20-18.ap-northeast-1.compute.amazonaws.com:3003/api',
   credential: false,
   localProvider: {
     1: process.env.VUE_APP_HP,
