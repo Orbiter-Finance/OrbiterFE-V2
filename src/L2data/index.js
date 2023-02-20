7833ac15efbec582b159b9ce482ff3b39d773df2
skywell <719779468@qq.com>
2023-02-20 19:05:15 +0800

    l2 base url

--- src/L2data/index.js
+++ src/L2data/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import {  isLocal } from '../util/env'
+import env from "../../env";
 
-const baseURL = isLocal() ? '/api/v1/l2_data/' : 'https://l2api.orbiter.finance/api/v1/l2_data'
+const baseURL = isLocal() ? '/api/v1/l2_data/' : `${ env.l2BaseUrl }/api/v1/l2_data`;
 
 export default axios.create({
   baseURL,
