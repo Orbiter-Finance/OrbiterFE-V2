c55dcfedd898c04ef5dcab78430fc62e1089ca0d
Eric <zu19890223@gmail.com>
2023-02-22 23:19:00 +0800

    to arb online

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1661,13 +1661,13 @@ export default {
           })
           return
         }
-        if (toChainID == 2) {
-          this.$notify.error({
-            title: `Arbitrum network maintenance is in progress, please try again later`,
-            duration: 3000,
-          })
-          return
-        }
+        // if (toChainID == 2) {
+        //   this.$notify.error({
+        //     title: `Arbitrum network maintenance is in progress, please try again later`,
+        //     duration: 3000,
+        //   })
+        //   return
+        // }
         if (fromChainID == 10) {
           this.$notify.error({
             title: `Metis network maintenance is in progress, please try again later`,
