919060d8221a20fa8d80a0890b6b23348aa70e8f
Eric <zu19890223@gmail.com>
2023-03-24 14:04:58 +0800

    offline to starkNet

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1457,13 +1457,13 @@ export default {
         //   });
         //   return;
         // }
-        // if (toChainID === 4) {
-        //   this.$notify.error({
-        //         title: 'Due to network issues, this feature is temporarily suspended.',
-        //         duration: 3000,
-        //     })
-        //     return
-        // }
+        if (toChainID === 4) {
+          this.$notify.error({
+                title: 'Due to network issues, this feature is temporarily suspended.',
+                duration: 3000,
+            })
+            return
+        }
         if (toChainID === 3) {
           this.$notify.error({
                 title: 'Due to network issues, this feature is temporarily suspended.',
