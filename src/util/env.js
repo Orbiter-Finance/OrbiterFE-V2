3bd1f25c2de7e0a09f47fae86ae4767fb04197e6
skywell <719779468@qq.com>
2023-03-31 17:19:30 +0800

    L2DataDev

--- src/util/env.js
+++ src/util/env.js
@@ -1,6 +1,7 @@
 export const isLocal = () => process.env.VUE_APP_ENV === 'local'
 export const isDev = () => process.env.VUE_APP_ENV === 'development'
 export const isProd = () => process.env.VUE_APP_ENV === 'production'
+export const isL2DataDev = () => process.env.VUE_APP_L2Data_ENV === 'development'
 
 // TODO: should check by code
 export const isWebSimulation = false
