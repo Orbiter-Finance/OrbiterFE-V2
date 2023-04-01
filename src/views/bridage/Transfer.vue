c19d86d55393be36bcd63eab1a2053051041bada
Eric <zu19890223@gmail.com>
2023-04-01 08:03:34 +0800

    offline zk era

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1488,13 +1488,13 @@ export default {
         //     })
         //     return
         // }
-        // if (toChainID === 14) {
-        //   this.$notify.error({
-        //         title: 'Due to network issues, this feature is temporarily suspended.',
-        //         duration: 3000,
-        //     })
-        //     return
-        // }
+        if (toChainID === 14) {
+          this.$notify.error({
+                title: 'Due to network issues, this feature is temporarily suspended.',
+                duration: 3000,
+            })
+            return
+        }
         
         if (!selectMakerConfig) return;
         const { fromChain } = selectMakerConfig;
