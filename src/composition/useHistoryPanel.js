093c02dd20d3d3c85d21884a60ecc1547f2a10f7
0xKakui <103978691+kakui-lau@users.noreply.github.com>
2023-04-04 19:49:01 +0800

    change history detail

--- src/composition/useHistoryPanel.js
+++ src/composition/useHistoryPanel.js
@@ -36,7 +36,21 @@ export function getTraddingHistory(isRefresh = false) {
 }
 export function setHistoryInfo(info = {}, isShowHistory = true) {
   historyPanelState.isShowHistory = isShowHistory
-  historyPanelState.historyInfo = info
+  // historyPanelState.historyInfo = info
+  historyPanelState.historyInfo = {
+        fromChainID: info.fromChain,
+        fromTimeStamp: info.fromTimeStampShow,
+        fromTxHash: info.fromHash,
+        makerAddress: info.replySender,
+        state: 0,
+        toChainID: info.toChain,
+        toTimeStamp: info.toTimeStampShow,
+        toTxHash: info.toHash,
+        tokenName:info.toToken,
+        userAddress: info.replyAccount,
+        userAmount: info.fromAmountValue,
+  }
+ 
 }
 
 export async function getTransactionsHistory(params = {}) {
@@ -58,7 +72,9 @@ export async function getTransactionsHistory(params = {}) {
         decimal = 6
       }
       const fromDate = new Date(row.fromTime)
+      const toDate = new Date(row.toTime)
       row.fromTimeStampShow = `${fromDate.toLocaleTimeString()} ${fromDate.toLocaleDateString()}`
+      row.toTimeStampShow = `${toDate.toLocaleTimeString()} ${toDate.toLocaleDateString()}`
       row.fromAmountValue = (row.fromAmount / 10 ** decimal).toFixed(8)
       return row
     })
@@ -70,77 +86,4 @@ export async function getTransactionsHistory(params = {}) {
   } finally {
     historyPanelState.isLoading = false
   }
-
-  // const res = await getTransactionsHistoryApi({
-  //   // next line is just for local test only
-  //   // userAddress: '0x6BB0366423a6f0F6C16715278483Dd9321ED5f66',
-  //   // userAddress: '0x8a700FdB6121A57C59736041D9aa21dfd8820660',
-  //   // users
-  //   // userAddress: '0x2f21191fb455d60018e0415adce926ee0fe3d67c',
-  //   // userAddress: '0x298cc3e53fea7ff5c9e5f943dd71529de16398ba',
-  //   userAddress: walletAddress,
-  //   size: 10,
-  //   ...params,
-  // })
-  // historyPanelState.transactionListInfo.current = params?.current || 1
-  // historyPanelState.isLoading = false
-
-  // if (res.data.code === 0) {
-  //   const { data = [], ...resInfo } = res.data
-  //   const list = data.map((v) => {
-  //     /*
-  //       {
-  //         "fromChainID": 33,
-  //         "toChainID": 5,
-  //         "userAddress": "0xbd...2415",
-  //         "makerAddress": "0x00...4719",
-  //         "userAmount": "0.008903",
-  //         "fromTimeStamp": "06-10 11:10",
-  //         "toTimeStamp": "06-10 11:10",
-  //         "tokenName": "ETH",
-  //         "fromTxHash": "0x9f9a408a24312b01006e14ebfc25792185dc3516cde28bf024ef7e09156f5233",
-  //         "toTxHash": "0xd5f57bf6f664ad5617e5f5ba1a7fba6d836ba6ff0c60201fe393a544f9730977",
-  //         "state": 0
-  //       }
-  //     */
-
-  //     let fromTimeStampShow = v.fromTimeStamp || ''
-  //     if (fromTimeStampShow) {
-  //       // 2022-07-22 09:24:56
-  //       const arr = fromTimeStampShow.split(/\W+/)
-  //       const y = arr[0]
-  //       const m = parseInt(arr[1]) - 1
-  //       const d = arr[2]
-  //       const h = arr[3]
-  //       const M = arr[4]
-  //       const s = arr[5]
-  //       fromTimeStampShow = formatDateShort(Date.UTC(y, m, d, h, M, s))
-  //     }
-  //     return {
-  //       fromChainID: +v.fromChain,
-  //       toChainID: +v.toChain,
-  //       userAddress:
-  //         v.userAddress.slice(0, 4) + '...' + v.userAddress.slice(-4),
-  //       makerAddress:
-  //         v.makerAddress.slice(0, 4) + '...' + v.makerAddress.slice(-4),
-  //       userAmount: v.fromValueFormat,
-  //       fromTimeStamp: v.fromTimeStamp,
-  //       fromTimeStampShow,
-  //       toTimeStamp: v.toTimeStamp,
-  //       tokenName: v.tokenName,
-  //       fromTxHash: v.fromTx,
-  //       toTxHash: v.toTx,
-  //       status: v.status,
-  //       state: (() => {
-  //         if (v.fromTx && v.toTx) {
-  //           return 0
-  //         }
-  //         if (v.status == 0) return 1
-  //         return 2
-  //       })(),
-  //     }
-  //   })
-  //   historyPanelState.transactionListInfo = resInfo
-  //   historyPanelState.transactionList = list
-  // }
 }
