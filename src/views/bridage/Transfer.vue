5031b61f5e274bd1370be81d1b7790abf754585d
Eric <zu19890223@gmail.com>
2023-05-10 08:29:47 +0800

    online starkNet

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1560,13 +1560,13 @@ export default {
                 fromChain.symbol,
                 compatibleGlobalWalletConf.value.walletPayload.walletAddress
         );
-        if (toChainID === 4 || toChainID === 44) {
-          this.$notify.error({
-            title: `The StarkNet network transaction maintenance, please try again later`,
-            duration: 6000,
-          });
-          return;
-        }
+        // if (toChainID === 4 || toChainID === 44) {
+        //   this.$notify.error({
+        //     title: `The StarkNet network transaction maintenance, please try again later`,
+        //     duration: 6000,
+        //   });
+        //   return;
+        // }
         // if (toChainID === 16 || toChainID === 516) {
         //   this.$notify.error({
         //     title: `The Arbitrum Nova network transaction maintenance, please try again later`,
