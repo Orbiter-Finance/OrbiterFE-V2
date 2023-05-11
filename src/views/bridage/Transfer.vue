e340d6feda18f1f193e805e03ed1c7a0d87417e1
Eric <zu19890223@gmail.com>
2023-05-12 01:14:31 +0800

    offline starkNet

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1546,13 +1546,13 @@ export default {
                 fromChain.symbol,
                 compatibleGlobalWalletConf.value.walletPayload.walletAddress
         );
-        // if (toChainID === 4 || toChainID === 44) {
-        //   this.$notify.error({
-        //     title: `The StarkNet network transaction maintenance, please try again later`,
-        //     duration: 6000,
-        //   });
-        //   return;
-        // }
+        if (toChainID === 4 || toChainID === 44) {
+          this.$notify.error({
+            title: `The StarkNet network transaction maintenance, please try again later`,
+            duration: 6000,
+          });
+          return;
+        }
         if (fromChainID === 7 && toChainID === 4) {
           this.$notify.error({
               title: `The optimism-starkNet network transaction maintenance, please try again later`,
