c5af853ab2de5c52e2d704241afbb292d60bbdc3
Eric <zu19890223@gmail.com>
2023-05-23 20:10:44 +0800

    online starkNet

--- src/views/bridage/Transfer.vue
+++ src/views/bridage/Transfer.vue
@@ -1531,13 +1531,13 @@ export default {
                 fromChain.symbol,
                 compatibleGlobalWalletConf.value.walletPayload.walletAddress
         );
-        if (toChainID === 4 || toChainID === 44) {
-          this.$notify.error({
-            title: `The StarkNet network transaction maintenance, please try again later`,
-            duration: 6000,
-          });
-          return;
-        }
+        // if (toChainID === 4 || toChainID === 44) {
+        //   this.$notify.error({
+        //     title: `The StarkNet network transaction maintenance, please try again later`,
+        //     duration: 6000,
+        //   });
+        //   return;
+        // }
         if (toChainID === 4 && fromChain.symbol == 'DAI') {
           this.$notify.error({
             title: `The StarkNet network transaction maintenance, please try again later`,
