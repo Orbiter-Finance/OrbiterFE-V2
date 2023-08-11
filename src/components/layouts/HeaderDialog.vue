eac865aa139f515e5573b372852eca2c278d838b
kakui <huanhui.boy@gmail.com>
2023-08-11 22:15:21 +0800

    change Bitget name

--- src/components/layouts/HeaderDialog.vue
+++ src/components/layouts/HeaderDialog.vue
@@ -187,7 +187,7 @@ export default {
                 {
                     isConnect: false,
                     icon: 'bitkeep',
-                    title: 'BitKeep',
+                    title: 'Bitget',
                 },
                 {
                     isConnect: false,
