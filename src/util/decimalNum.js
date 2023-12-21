fc320f97133d99e26dafdc3b76485afaf6f38b08
developer-otto <developer.otto@outlook.com>
2023-12-21 13:26:09 +0800

    fix:History Number decimal

---
+++ src/util/decimalNum.js
@@ -0,0 +1,45 @@
+export const decimalNum = (
+  num = 0,
+  decimal,
+  delimiter = '',
+  currencySymbol = ''
+) => {
+  decimal = Number(decimal) || 2
+
+  const big = num ? num.toString() : '0'
+
+  console.log(big, '1111')
+
+  const negativeNChar = Number(big) < 0 ? '-' : ''
+
+  const bigArr = (Number(big) < 0 ? big.slice(1) : big).toString().split('.')
+  let intStr = ''
+  let decStr = bigArr[1]?.slice(0, Number(decimal)) || ''
+
+  while (!Number(decStr[decStr.length - 1]) && !!decStr) {
+    decStr = decStr.slice(0, decStr.length - 1)
+  }
+  bigArr[0]
+    .split('')
+    .reverse()
+    .forEach((item, idx) => {
+      if (!!idx && !(idx % 3) && idx !== bigArr[0].length) {
+        intStr = item + delimiter + intStr
+      } else {
+        intStr = item + intStr
+      }
+    })
+
+  if (Number(decimal)) {
+    if (decStr) {
+      intStr += '.' + decStr
+    }
+  }
+
+  console.log(
+    'currencySymbol + negativeNChar + intStr',
+    currencySymbol + negativeNChar + intStr
+  )
+
+  return currencySymbol + negativeNChar + intStr
+}
