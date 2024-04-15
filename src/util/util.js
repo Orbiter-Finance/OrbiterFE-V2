23ad40b689ea677a57121430da2047351c440326
developer-otto <developer.otto@outlook.com>
2024-04-15 17:48:47 +0800

    feat: add x layer network logo

--- src/util/util.js
+++ src/util/util.js
@@ -82,7 +82,11 @@ export default {
     })
   },
   netWorkName(networkId) {
-    return this.getV3ChainInfoByChainId(networkId)?.name || 'Unknown Network'
+    return (
+      this.getV3ChainInfoByChainId(networkId)?.name ||
+      networkId ||
+      'Unknown Network'
+    )
   },
   chainName(chainId) {
     return this.getV3ChainInfoByChainId(chainId)?.name || chainId || ''
