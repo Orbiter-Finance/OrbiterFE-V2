55f194f540714da9ace29eca7cdc1b08b451f564
developer-otto <developer.otto@outlook.com>
2024-06-05 22:14:54 +0800

    fix: draw orbguy path

--- src/common/openApiAx.js
+++ src/common/openApiAx.js
@@ -110,11 +110,11 @@ export async function getNoticeData() {
 }
 
 export async function requestClaimLuckyBagReward(address) {
-  return await openApiAx.get(`/points-platform/reward/address/${address}`)
+  return await openApiAx.get(`/points_platform/reward/address/${address}`)
 }
 
 export async function drawClaimLuckyBagReward(address) {
-  return await openApiAx.post(`/points-platform/reward/draw`, {
+  return await openApiAx.post(`/points_platform/reward/draw`, {
     address,
   })
 }
