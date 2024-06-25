490040912d0120f19e11c41ca4e1086344394957
developer-otto <developer.otto@outlook.com>
2024-06-25 18:16:36 +0800

    fix: api

--- src/views/prizes/components/PrizesClaimCard.vue
+++ src/views/prizes/components/PrizesClaimCard.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="prizes-claim-card">
     <div class="card-title">Congratulations 🎉</div>
-    <div class="ranking-info info">
+    <!-- <div class="ranking-info info">
       <div class="label">Current rank</div>
       <div class="amount">19</div>
-    </div>
+    </div> -->
     <div class="tx-info info">
       <div class="label">Accumulated</div>
-      <div class="amount">1592tx bridges</div>
+      <div class="amount">{{ txAmount }}tx bridges</div>
     </div>
     <div class="reward-card-group">
-      <div class="reward-card">
-        <div class="usdc-symbol">
-          <svg-icon iconName="USDC"></svg-icon>
+      <div class="reward-group">
+        <div class="reward-card" v-for="item in list" :key="item.symbol">
+          <div class="usdc-symbol">
+            <svg-icon :iconName="item.symbol"></svg-icon>
+          </div>
+          <div class="reward-amount"> {{ item.quantity }} $USDC</div>
         </div>
-        <div class="reward-amount">+1,478.84 $USDC</div>
-        <div class="claim-btn">Claim</div>
       </div>
-      <div class="reward-card">
-        <div class="usdc-symbol">
-          <svg-icon iconName="ORBGUY"></svg-icon>
-        </div>
-        <div class="reward-amount">+1,478 $ORBGUY</div>
-        <div class="claim-btn">Claim</div>
+
+     <div class="claim-group">
+      <div class="claim-btn">
+        Claim
       </div>
+     </div>
     </div>
     <div class="desction">
       Claim your Token on <span class="tips">Arbitrum</span>
@@ -32,8 +32,60 @@
 </template>
 
 <script>
+
+import { compatibleGlobalWalletConf } from '../../../composition/walletsResponsiveData'
+import {requestClaimPrizesRewardData  } from "../../../common/openApiAx"
+import { decimalNum } from '../../../util/decimalNum'
+import {
+  prizesUserTx
+} from '../../../composition/hooks'
+
 export default {
   name: 'PrizesClaimCard',
+  data() {
+    return {
+      list: [{
+      symbol: "USDC",
+      quantity: "--"
+    }]
+    }
+  },
+  computed: {
+    tx() {
+      return prizesUserTx.value
+    },
+    txAmount() {
+      return this.decimalNumC(this.tx || "0", 0, ",")
+    },
+    evmAddress() {
+      return compatibleGlobalWalletConf.value.walletPayload.walletAddress || ''
+    },
+  },
+  watch: {
+    evmAddress(item1, item2) {
+      if (!!item1 && item1 !== item2) {
+        this.getData()
+      }
+    },
+  },
+  methods: {
+    decimalNumC(num, decimal, delimiter, symbol) {
+      return decimalNum(num, decimal, delimiter, symbol)
+    },
+    async getData() {
+      if (!this.evmAddress || this.evmAddress === "0x") return
+      const res = await requestClaimPrizesRewardData(
+        this.evmAddress
+      )
+      console.log("1111", res )
+      this.list = res.map((item)=>{
+        return ({
+          ...item,
+          quantity: this.decimalNumC(item.quantity, "6", ",")
+        })
+      })
+    }
+  }
 }
 </script>
 
@@ -75,40 +127,48 @@ export default {
   }
   .reward-card-group {
     width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: start;
-    .reward-card {
-      width: 48%;
-      margin-top: 16px;
-      border-radius: 12px;
-      background: rgb(34, 34, 34);
-      padding: 24px 16px;
-      .usdc-symbol {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        svg {
-          width: 64px;
-          height: 64px;
+    background: rgb(34, 34, 34);
+    border-radius: 12px;
+    margin-top: 16px;
+
+    .reward-group {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: start;
+      .reward-card {
+        width: 48%;
+        padding: 24px 16px;
+        .usdc-symbol {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          svg {
+            width: 64px;
+            height: 64px;
+          }
+        }
+        .reward-amount {
+          width: 100%;
+          margin-top: 12px;
+          color: rgb(255, 255, 255);
+          font-size: 20px;
+          font-weight: 700;
+          line-height: 32px;
+          letter-spacing: 0px;
+          text-align: center;
+          font-family: GeneralSans-SemiBold;
+          white-space: nowrap;
         }
       }
-      .reward-amount {
-        width: 100%;
-        margin-top: 12px;
-        color: rgb(255, 255, 255);
-        font-size: 20px;
-        font-weight: 700;
-        line-height: 32px;
-        letter-spacing: 0px;
-        text-align: center;
-        font-family: GeneralSans-SemiBold;
-        white-space: nowrap;
-      }
+    }
+
+    .claim-group {
+      width: 100%;
+      padding: 12px 16px;
       .claim-btn {
         width: 100%;
-        margin-top: 12px;
         border-radius: 8px;
         background: rgb(223, 46, 45);
         padding: 12px 0;
