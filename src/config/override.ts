b1c71916e40dc257b8ae4477d48dcb66249f8f59
kakui <huanhui.boy@gmail.com>
2024-08-27 11:10:48 +0800

    change rule

--- src/config/override.ts
+++ src/config/override.ts
@@ -2,7 +2,7 @@ const allRules = require('./eth-e4e-prod.json');
 const changeFromRules = require('./0x3472-prod.json');
 for (const chain in changeFromRules) {
     for (const symbol in changeFromRules[chain]) {
-        const findData = changeFromRules[chain][symbol];
+        const findData = allRules[chain][symbol];
         if (findData) {
             changeFromRules[chain][symbol].gasFee = findData.gasFee;
             changeFromRules[chain][symbol].tradingFee = findData.tradingFee;
