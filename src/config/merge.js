688d7dfc824fe4494df65d33cfdbf53076426070
kakui <huanhui.boy@gmail.com>
2024-10-23 12:06:25 +0800

    change 80c fee

--- src/config/merge.js
+++ src/config/merge.js
@@ -8,29 +8,38 @@ const l2Maker =
 const l1AfterMaker = '0x80c67432656d59144ceff962e8faf8926599bcf8'
 const l2AfterMaker =
   '0x07b393627bd514d2aa4c83e9f0c468939df15ea3c29980cd8e7be3ec847795f0'
-for (const chain in fromRule) {
-  for (const symbol in fromRule[chain]) {
-    const rule = fromRule[chain][symbol]
-    if (!toRule[chain]) {
-      console.log('ChainId not found', chain)
-      toRule[chain] = {}
-    }
-    if (!toRule[chain][symbol]) {
-      console.log('Rule not found', chain, symbol)
-      for (const k in rule) {
-        if (
-          String(rule[k]).toLocaleLowerCase() === l1Maker.toLocaleLowerCase()
-        ) {
-          rule[k] = l1AfterMaker
-        }
-        if (
-          String(rule[k]).toLocaleLowerCase() === l2Maker.toLocaleLowerCase()
-        ) {
-          rule[k] = l2AfterMaker
-        }
-      }
-      toRule[chain][symbol] = rule
+for (const chain in toRule) {
+  for (const symbol in toRule[chain]) {
+    console.log(chain, '====', symbol)
+    if (fromRule[chain][symbol]) {
+      const fromItem = fromRule[chain][symbol];
+      toRule[chain][symbol]['gasFee'] = fromItem.gasFee;
+      toRule[chain][symbol]['tradingFee'] = fromItem.tradingFee;
     }
   }
+  console.log(JSON.stringify(toRule));
+  // for (const symbol in fromRule[chain]) {
+  //   const rule = fromRule[chain][symbol]
+  //   if (!toRule[chain]) {
+  //     console.log('ChainId not found', chain)
+  //     toRule[chain] = {}
+  //   }
+  //   if (!toRule[chain][symbol]) {
+  //     console.log('Rule not found', chain, symbol)
+  //     for (const k in rule) {
+  //       if (
+  //         String(rule[k]).toLocaleLowerCase() === l1Maker.toLocaleLowerCase()
+  //       ) {
+  //         rule[k] = l1AfterMaker
+  //       }
+  //       if (
+  //         String(rule[k]).toLocaleLowerCase() === l2Maker.toLocaleLowerCase()
+  //       ) {
+  //         rule[k] = l2AfterMaker
+  //       }
+  //     }
+  //     toRule[chain][symbol] = rule
+  //   }
+  // }
 }
 // console.log(JSON.stringify(toRule), '=fromRule')
